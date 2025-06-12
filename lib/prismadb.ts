2a70290d9b1b735135d0ad0ce1bb09931204fb5f
chakunde2004 <om.chakunde@mitaoe.ac.in>
2025-06-12 14:17:22 +0530

    fix: remove fs usage for Vercel compatibility

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,11 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 require('dotenv').config(); // Load environment variables from .env
 
-const fs = require('fs');
-console.log("Current Directory:", process.cwd()); // Log the current working directory
-console.log(".env File Exists:", fs.existsSync('.env')); // Check if the .env file exists
-console.log("DATABASE_URL:", process.env.DATABASE_URL); // Debug the DATABASE_URL
-
 declare global {
   var prisma: PrismaClient | undefined;
 }
